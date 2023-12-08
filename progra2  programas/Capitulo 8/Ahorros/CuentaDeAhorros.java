9d4194a0fd4c082b67337988365eb14e313d7845
Blancops2 <140431876+Blancops2@users.noreply.github.com>
2023-12-07 17:05:59 -0800

    Add files via upload

---
+++ progra2  programas/Capitulo 8/Ahorros/CuentaDeAhorros.java
@@ -0,0 +1,21 @@
+public class CuentaDeAhorros {
+    private static double tasaInteresAnual;
+    private double saldoAhorros;
+
+    public CuentaDeAhorros(double saldoInicial) {
+        saldoAhorros = saldoInicial;
+    }
+
+    public void calcularInteresMensual() {
+        double interesMensual = (saldoAhorros * (tasaInteresAnual / 12)) / 100.0;
+        saldoAhorros += interesMensual;
+    }
+
+    public static void modificarTasaInteres(double nuevaTasa) {
+        tasaInteresAnual = nuevaTasa;
+    }
+
+    public double obtenerSaldoAhorros() {
+        return saldoAhorros;
+    }
+}
